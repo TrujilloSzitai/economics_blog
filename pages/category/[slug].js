99b6c547a2e1bbb05fa952bc1753c4609ee2c6de
Francisco Trujillo <trujilloszitai@gmail.com>
2022-08-28 14:57:15 -0300

    Correccion en categories/[slug].js

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -14,7 +14,7 @@ const PostsPerCategory = ({ posts }) => {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
-          {posts.lenght > 0 ? (
+          {posts.length > 0 ? (
             posts.map((post, index) => (
               <PostCard key={index} post={post.node} />
             ))
@@ -24,7 +24,7 @@ const PostsPerCategory = ({ posts }) => {
                 <img
                   src="/assets/img/page-not-found.svg"
                   alt='Aún no hay artículos publicados'
-                  className="object-top h-full w-full   "
+                  className="object-top h-full w-full"
                 />
               </div>
               <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
